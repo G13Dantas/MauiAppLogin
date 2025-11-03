f5b3b769e73ffde8581dc3ea457a915fb5fc2333
Dantas <gdantasestudos01@gmail.com>
2025-11-02 21:49:15 -0300

    Add arquivos ao projeto - Dantas 1

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,14 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+        protected override Window CreateWindow(IActivationState activationState) 
+        { 
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            //Janela no formato de celular
+            return window;
+
+        }
+    }//Fecha classe
+}//Fecha namespace
